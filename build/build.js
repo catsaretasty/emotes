8d2b235f77ea504232fcc729ddaa85e99fefcc88
chip <chip@chip.sh>
2016-02-21 14:48:34 -0700

    convert build script to es6

--- build/build.js
+++ build/build.js
@@ -1,45 +1,42 @@
-var path = require('path');
-var Promise = require('bluebird');
-var imageSize = require('image-size');
-var recursive = require('recursive-readdir');
-var fs = Promise.promisifyAll(require('fs-extra'));
+"use strict";
 
-var base_url = 'https://emotes.tastycat.org/';
+const path = require('path');
+const Promise = require('bluebird');
+const imageSize = require('image-size');
+const recursive = require('recursive-readdir');
+const fs = Promise.promisifyAll(require('fs-extra'));
 
-var output_dir = './public/';
+const base_url = 'https://emotes.tastycat.org/';
 
-var emote_list = {emotes: {}};
-var emote_list_full = {emotes: {}};
-var emote_list_data = [];
+const output_dir = './public/';
+
+const emote_list = {emotes: {}};
+const emote_list_full = {emotes: {}};
+const emote_list_data = [];
 
 function getEmoteFiles() {
-    var deferred = Promise.defer();
+    const deferred = Promise.defer();
     // ignore files that end with *.txt
-    recursive('./emotes', ['*.txt'], function (err, files) {
+    recursive('./emotes', ['*.txt'], (err, files) => {
         deferred.resolve(files);
     });
     return deferred.promise;
 }
 
 function matchEmoteAltnames(files) {
-    var promises = [];
-    for (var i = 0; i < files.length; i++) {
-        var f = Promise.try(function (file) {
-            return file;
-        }, files[i]);
+    const promises = [];
+    for (let i = 0; i < files.length; i++) {
+        const f = Promise.try(file => file, files[i]);
 
-        promises.push(f.then(function (f) {
-            return Promise.all([f, doesFileExist(f)]);
-        }));
+        promises.push(f.then(f => Promise.all([f, doesFileExist(f)])));
     }
     return Promise.all(promises);
 }
 
 function doesFileExist(f) {
-    return fs.statAsync(f + '.txt')
-        .then(function (stat) {
-            return stat.isFile();
-        }).catch(function (e) {
+    return fs.statAsync(`${f}.txt`)
+        .then(stat => stat.isFile())
+        .catch(e => {
             if (e.code !== 'ENOENT') {
                 console.error('file error: ', e.code)
             }
@@ -48,45 +45,33 @@ function doesFileExist(f) {
 }
 
 function getEmotes(emote_promises) {
-    var promises = [];
-    for (var i = 0; i < emote_promises.length; i++) {
-        var emote_promise = emote_promises[i];
+    const promises = [];
+    for (let i = 0; i < emote_promises.length; i++) {
+        const emote_promise = emote_promises[i];
         promises.push(Promise.all(getEmote(emote_promise[0], emote_promise[1])))
     }
     return Promise.all(promises);
 }
 
 function getEmote(file, alt_name_exists) {
-    var name;
-    if (alt_name_exists) {
-        name = fs.readFileAsync(file + '.txt')
-    } else {
-        name = Promise.try(function (name) {
-            return name;
-        }, path.parse(file).name);
-    }
-
-    file = Promise.try(function (file) {
-        return file;
-    }, file);
-
+    const name = alt_name_exists ? fs.readFileAsync(`${file}.txt`) : path.parse(file).name;
     return [file, name];
 }
 
 function makeEmoteLists(emotes) {
-    for (var i = 0; i < emotes.length; i++) {
-        var emote = {file: emotes[i][0], name: emotes[i][1].toString()};
+    for (let i = 0; i < emotes.length; i++) {
+        const emote = {file: emotes[i][0], name: emotes[i][1].toString()};
 
         // default small payload is just a url string
         emote.payload = base_url + emote.file;
         addToEmoteList(emote, emote_list);
 
         // full payload is an object containing more info, like file dimensions
-        var dims = imageSize(emote.file);
+        const dims = imageSize(emote.file);
         emote.payload = {url: emote.payload, height: dims.height, width: dims.width};
         addToEmoteList(emote, emote_list_full);
 
-        var emote_data = {
+        const emote_data = {
             name: emote.name,
             url: base_url + emote.file,
             height: dims.height,
@@ -95,11 +80,12 @@ function makeEmoteLists(emotes) {
 
         emote_list_data.push(emote_data);
 
-        fs.copyAsync(emote.file, output_dir + emote.file).then(function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
+        fs.copyAsync(emote.file, output_dir + emote.file)
+            .then(err => {
+                if (err) {
+                    console.error(err);
+                }
+            });
     }
     writeEmoteList(emote_list, 'emotes.json');
     writeEmoteList(emote_list_full, 'emotes-full.json');
@@ -116,23 +102,12 @@ function addToEmoteList(emote, emote_list) {
     }
 }
 
-function writeEmoteList(emote_list, output_name) {
-    return fs.outputJsonAsync(output_dir + output_name, emote_list, {spaces: 0})
-}
-
-fs.ensureDirAsync(output_dir)
-    .then(getEmoteFiles)
-    .then(matchEmoteAltnames)
-    .then(getEmotes)
-    .then(makeEmoteLists)
-    .then(makeEmoteDisplayPage);
-
 function makeEmoteDisplayPage(emotes) {
-    var jade = require('jade');
+    const jade = require('jade');
 
-    var css = fs.readFileSync(output_dir + 'emotes.css');
-    var display = jade.renderFile('app/index.jade', {emotes: emotes, css: css});
-    var noscript_display = jade.renderFile('app/noscript.jade', {emotes: emotes, css: css});
+    const css = fs.readFileSync(`${output_dir}emotes.css`);
+    const display = jade.renderFile('app/index.jade', {emotes, css});
+    const noscript_display = jade.renderFile('app/noscript.jade', {emotes, css});
 
     writePublicFile('index.html', display);
     writePublicFile('noscript/index.html', noscript_display);
@@ -140,12 +115,23 @@ function makeEmoteDisplayPage(emotes) {
     return display;
 }
 
-var writePublicFile = function(file, content) {
+function writeEmoteList(emote_list, output_name) {
+    return fs.outputJsonAsync(output_dir + output_name, emote_list, {spaces: 0})
+}
+
+var writePublicFile = (file, content) => {
     fs.outputFileAsync(output_dir + file, content)
-        .then(function (err) {
+        .then(err => {
             if (err) {
                 console.error(err);
             }
-            console.log('wrote ' + output_dir + file + '!');
+            console.log(`wrote ${output_dir}${file}!`);
         });
 };
+
+fs.ensureDirAsync(output_dir)
+    .then(getEmoteFiles)
+    .then(matchEmoteAltnames)
+    .then(getEmotes)
+    .then(makeEmoteLists)
+    .then(makeEmoteDisplayPage);
